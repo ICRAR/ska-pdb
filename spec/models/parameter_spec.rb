75c732edad199fa22f29c6bfc4907d252bea4d0d
Tobias Lauchenauer <tobias@lauchenauer.info>
2012-05-29 12:29:16 +0800

    adjusted tests to match the new full text search

--- spec/models/parameter_spec.rb
+++ spec/models/parameter_spec.rb
@@ -1,39 +1,38 @@
 require 'spec_helper'
 
-describe Parameter do
-  before :all do
+describe "Parameter" do
+  before(:each) do
     @total_parameters = 36
     @max_results_per_page = 20
+    Parameter.delete_all
     @total_parameters.times do |index|
-      Parameter.create(description: "description #{index}", unit: "unit #{index}", source: "source #{index}", expression: "expression #{index}")
+      Parameter.create(description: "description #{index}", source: "source #{index}")
     end
   end
         
-  describe "Using an empty search filter with a page size greater than the number of paramaters"
+  describe "Using an empty search filter with a page size greater than the number of paramaters" do
     it "should return all of the parameters", :integration => true do
       results = Parameter.search 1, 50
       results.size.should == @total_parameters
     end
+  end
     
-  describe "Using a valid search filter"
-    it "should return results filtered by one criteria", :integration=>true do
-      results  = Parameter.search 1, 20, SearchFilter.initialize_from({"unit" => "4"})
-      results.size.should == 4
+  describe "Using a valid search filter" do
+    it "should return results filtered by text", :integration=>true do
+      results  = Parameter.search 1, 20, SearchFilter.initialize_from({"text" => "description 1"})
+      results.size.should == 13
     end
+  end
     
-    it "should return results filtered by two criteria", :integration=>true do
-      results  = Parameter.search 1, 20, SearchFilter.initialize_from({"unit" => "1", "description" => "5"})
-      results.size.should == 1
-    end
-    
-  describe "Using pagination"
+  describe "Using pagination" do
     it "should return the max number of results in a page", :integration=>true do
       results = Parameter.search 2, 20
       results.size.should == @max_results_per_page
     end
 
     it "should return results filtered in a page", :integration=>true do
-      results = Parameter.search 1, 20, SearchFilter.initialize_from({"unit" => "1"})
+      results = Parameter.search 1, 20, SearchFilter.initialize_from({"text" => "1"})
       results.size.should == 13
     end
+  end
 end
