dac5cc04062664252c8aeef49419a20eaac63f74
Clarence Bakirtzidis <clarenceb@gmail.com>
2012-09-10 22:44:58 +1000

    Fixed failing spec
    * More work needs to be done on thse specs

--- spec/controllers/parameters_controller_spec.rb
+++ spec/controllers/parameters_controller_spec.rb
@@ -4,14 +4,63 @@ describe ParametersController do
   before(:each) do
     controller.stub(:user_signed_in?).and_return true
   end
-    
+
   describe "GET index" do
-    it "should return all parameters" do
-      Parameter.stub(:paginate).with(:page => "test.page", :per_page => 20) {:some_params}
+    describe "instance variables" do
+      let(:search_text) { nil }
+      let(:params) { {:text => search_text} }
+      before { get :index, params }
 
-      get :index, {:page => "test.page"}
+      it { assigns(:cart).should be_true }
+      it { assigns(:search).should be_false }
+      it { assigns(:search_text).should be_false }
+      it { assigns(:search_terms).should be_false }
 
-      assigns(:parameters).should == :some_params
+      describe "when searching" do
+        let(:search_text) { "some text" }
+
+        it { assigns(:search).should be_true }
+        it { assigns(:search_text).should == "some text" }
+        it { assigns(:search_terms).should == ["some", "text"] }
+      end
+    end
+
+    describe "html format" do
+      it "should render index template" do
+        get :index
+
+        response.should render_template('parameters/index')
+      end
+    end
+
+    describe "json format" do
+      it "should create a parameters query helper" do
+        expected_hash = {
+          :user_signed_in => true,
+          :page_size => 20,
+          :search => nil,
+          :search_text => nil,
+          :search_terms => nil }
+        ParametersDatatable.should_receive(:new).with(anything, expected_hash)
+
+        get :index, :format => :json
+      end
+
+      it "should return json results of the query"
+
+      context "when searching" do
+        it "should create a parameters query helper with search parameters" do
+          expected_hash = {
+            :user_signed_in => true,
+            :page_size => 20,
+            :search => true,
+            :search_text => "some text",
+            :search_terms => ["some", "text"] }
+          ParametersDatatable.should_receive(:new).with(anything, expected_hash)
+
+          get :index, { :text => "some text", :format => :json }
+        end
+      end
     end
   end
 
